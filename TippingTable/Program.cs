e466300fe1c7ff74e0f32f447b5636cfd6eb987b
kgk5377 <43886659+kgk5377@users.noreply.github.com>
2018-10-06 07:33:42 -0700

    Add project files.

---
+++ TippingTable/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using static System.Console;
+
+namespace TippingTable
+{
+    class TippingTable
+    {
+        static void Main(string[] args)
+        {
+            double dinnerPrice = 10.00;
+            double tipRate;
+            double tip;
+
+            const double LOWRATE = 0.10;
+            const double MAXRATE = 0.25;
+            const double TIPSTEP = 0.05;
+            const double MAXDINNER = 100.00;
+            const double DINNERSTEP = 10.00;
+
+            Write("   Price");
+            for(tipRate = LOWRATE; tipRate <= MAXRATE; tipRate += TIPSTEP)
+            {
+                Write("{0, 8}", tipRate.ToString("F"));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------");
+
+            tipRate = LOWRATE;
+            
+            while (dinnerPrice <= MAXDINNER)
+            {
+                Write("{0, 8}", dinnerPrice.ToString("C"));
+                while (tipRate <= MAXRATE)
+                {
+                    tip = dinnerPrice * tipRate;
+                    Write("{0, 8}", tip.ToString("F"));
+                    tipRate += TIPSTEP;
+                }
+                dinnerPrice += DINNERSTEP;
+                tipRate = LOWRATE;
+                Console.WriteLine();
+
+            }
+
+            ReadLine();
+
+
+        }
+    }
+}
